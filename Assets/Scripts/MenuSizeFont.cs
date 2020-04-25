ec78dc6b10371e524d88ffd7bcc6da5d8aa65fb2
Denis Ignatov <danquimby@gmail.com>
2020-04-25 15:33:51 +0300

    add fix gui

--- Assets/Scripts/MenuSizeFont.cs
+++ Assets/Scripts/MenuSizeFont.cs
@@ -8,8 +8,6 @@ public class MenuSizeFont : MonoBehaviour
     [SerializeField] private GameObject[] textTop;
     void Start()
     {
-        Debug.Log("width " + Screen.width);
-        Debug.Log("height " + Screen.height);
         foreach (GameObject o in textTop)
         {
             o.GetComponent<Text>().fontSize = Screen.width / 10;    
