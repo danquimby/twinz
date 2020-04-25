5f5c43bfd4bfddfc64c54f33d0a9bffa31063dfe
Denis Ignatov <danquimby@gmail.com>
2020-04-25 15:08:44 +0300

    gui

---
+++ Assets/Scripts/MenuSizeFont.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuSizeFont : MonoBehaviour
+{
+    [SerializeField] private GameObject[] textTop;
+    void Start()
+    {
+        Debug.Log("width " + Screen.width);
+        Debug.Log("height " + Screen.height);
+        foreach (GameObject o in textTop)
+        {
+            o.GetComponent<Text>().fontSize = Screen.width / 10;    
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
